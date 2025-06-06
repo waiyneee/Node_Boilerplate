afdfe9ef3e66a78c2ed9054d3b31e9705dfbf190
waiyneee <dikshkaushik1111@gmail.com>
2025-06-06 22:11:29 +0530

    added postman testing

--- RestApi/index.js
+++ RestApi/index.js
@@ -1,7 +1,15 @@
 const express = require("express")
 const app = express()
-const users = require("./MOCK_DATA.json")
+const usersData = require("./MOCK_DATA.json")
+const users=usersData.users
+
 const PORT = 3000
+const file=require("fs")
+
+
+//when i do post request mein req.body logg then it is undefined so i need  middleware  for that 100percent
+app.use(express.urlencoded({extended:false})) //xx-urlencode form data handle 
+//middleware ---->assume it as a plugin
 
 // Routes 
 app.get("/users", (req, res) => {
@@ -28,8 +36,15 @@ app.get("/api/users", (req, res) => {
 
 
 app.post("/api/users",(req,res)=>{
-    //creating new user
-    return res.json({status:"pending"})
+    //creating new user----->
+    const body=req.body;
+
+    users.push({...body,id:users.length+1}) //1001
+    // console.log("tyhe body is :" ,body);
+    file.writeFile("./MOCK_DATA.json",JSON.stringify({users}),(err,result)=>{
+        return res.json({status:"successful creation of user",id:users.length}) //1002
+    })
+    
 })
 
 
@@ -50,12 +65,42 @@ app.route("/api/users/:id").get((req, res) => {
 
 }).patch((req,res)=>{
     //edit the user
-    return res.json({status:"pending"});
+    const id= Number(req.params.id);
+    const body=req.body;
+    const userIdx= users.findIndex((user) =>user.id===id);
+    if(userIdx===-1) return res.json({status:"user does not exist"})
+    
+    users[userIdx]={...users[userIdx],...body};
+    //propogate changes in postman
+    file.writeFile("./MOCK_DATA.json",JSON.stringify({users}),(err,result)=>{
+        if(err){
+            return res.json({status:"unable to edit the user"})
+        }else{
+            return res.json({status:"user got edited successfully"});
+        }
+    })
+    
 
 
 }).delete((req,res)=>{
     //delete the user
-    return res.json({status:"pending"})
+
+    const body=req.body;
+    const id=Number(req.params.id )
+    const userIdx= users.findIndex(((user)=>user.id===id)) 
+    if(userIdx===-1) return res.json({status:"user cannot be found"});
+
+    users.splice(userIdx,1); //deleting that guy from array 1 is basicaly count coz ther may be multiple users 
+
+
+
+    file.writeFile("./MOCK_DATA.json",JSON.stringify({users}),(err,result)=>{
+        if(err){
+            return res.json({status:"unable to delete the user"})
+        }else{
+            return res.json({status:"user got delted successfully"})
+        }
+    })
 
 })
 
